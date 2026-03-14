d2adec1a117449bb93f9451a96f6dc014ed74e4a
Srivathsan Madhavan <m.srivathsan@gmail.com>
2026-03-14 10:43:34 +0530

    Business logic implemented

--- Core/ClaimSettlementService.cs
+++ Core/ClaimSettlementService.cs
@@ -14,6 +14,45 @@ public class ClaimSettlementService : IClaimSettlementService {
     public async Task<SettlementDecision> EvaluateClaimAsync(ClaimRequest request,
         CancellationToken cancellationToken)
     {
-        return await Task.FromResult(new SettlementDecision("", false, "invalid claim"));
+        if (policyAdminClient == null) {
+            return await Task.FromResult(
+                    new SettlementDecision("", false, "Policy Admin System inaccessible")
+                );
+        }
+        
+        // Create a "watchdog" task that completes when the token is cancelled
+        Task cancellationTask = Task.Delay(-1, cancellationToken);
+        
+        // Supposedly a long-running Task
+        Task<PolicyDetails> policyDetailsTask =
+            policyAdminClient.GetPolicyAsync(request.PolicyNumber, cancellationToken);
+        
+        var completedTask = Task.WhenAny(policyDetailsTask, cancellationTask);
+        if (completedTask == cancellationTask) {
+            throw new OperationCanceledException(cancellationToken);
+        }
+        
+        PolicyDetails det = await policyDetailsTask; // fetch Policy Details
+        var statusReason = det.IsActive ? "" : "Policy is inactive";
+
+        bool isWithinSmartSettleThreshold = det.IsActive;
+        if (isWithinSmartSettleThreshold) {
+            isWithinSmartSettleThreshold =
+                request.PropertyAgeYears <= 30 && request.ClaimAmount < 3000m;
+            if (!isWithinSmartSettleThreshold) {
+                isWithinSmartSettleThreshold =
+                    request.PropertyAgeYears > 30 && request.ClaimAmount < 1000m;
+            }
+            
+            if (!isWithinSmartSettleThreshold)
+                statusReason = "Smart Settle Threshold breached";
+        }
+        
+        return await Task.FromResult(new SettlementDecision(
+                                            request.PolicyNumber,
+                                            isWithinSmartSettleThreshold,
+                                            statusReason
+                                        )
+                                     );
     }
 }
--- Endpoints/Init.cs
+++ Endpoints/Init.cs
@@ -16,7 +16,7 @@ public static class AppEndpoints {
 
         // A simple GET endpoint returning a JSON object
         app.MapGet("/status", () => new { 
-            System = "Home Claims Automation",
+            System = "Home Claims Automation Service",
             Version = "v0.0.1",
             Os = "Linux Mint 22.3", 
             Framework = ".NET 10", 
@@ -32,7 +32,16 @@ public static class AppEndpoints {
         IPolicyClient cli = new DummyPolicyAdminClient(policyAdminUrl);
         IClaimSettlementService settlement = new ClaimSettlementService(cli);
         
-        return await Task.FromResult(Results.Created($"Claim received - {req}", req));
-        //return await Task.FromResult(Results.BadRequest($"Cannot process Claim - {req}"));
+        // Create a "watchdog" task that completes when the token is cancelled
+        Task cancellationTask = Task.Delay(-1, ct);
+        
+        // Supposedly a long-running Task
+        Task<SettlementDecision> settlementTask = settlement.EvaluateClaimAsync(req, ct);
+        
+        var completedTask = Task.WhenAny(settlementTask, cancellationTask);
+        if (completedTask == cancellationTask) {
+            throw new OperationCanceledException(ct);
+        }
+        return Results.Ok(await settlementTask);
     }
 }
